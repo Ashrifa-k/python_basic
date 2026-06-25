b955496fad9df7753eba5fc57c09de898e5e633c
ashrifak <ashrifaash06@gmail.com>
2026-06-25 16:55:24 +0530

    python_basic

---
+++ text.py
@@ -0,0 +1,28 @@
+print("hello world","hello world","hello world",sep="\n")
+#print("hello world",end="")
+#print("hello wolrd")
+x="10.5"
+# y="s"
+# print("abc">"abd")
+
+x=10.5
+print(int(float(x)))
+print ("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+print("""hello
+      my name is 
+      John
+      """)
+# l=[1,2,3,4,5]
+#print(l[::2])#start:end:step
+for i in range(1,10):
+    if i%2==0:
+        print(i)
+    #while True:
+    #   print("hello")
+
+for i in range (0,10,2):
+    print(i)
+#while True:
+ #   print("hello")   
